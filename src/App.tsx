8006b080f894531bc6725b7e4c4586a77ecea0b8
Təvəkkül <tevizeynalov@gmail.com>
2022-07-21 01:42:16 +0400

    create new esbuild pack

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './index.css';
+// @ts-ignore
+import Logo from './logo.png';
+
+const App = () => {
+
+    return (
+        <div>
+            hello react
+        </div>
+    );
+};
+
+export default App;
